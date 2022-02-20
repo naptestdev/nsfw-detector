38229b7077603d95bbe4085f942ae88bba1a4e63
napthedev <phongna.dev@gmail.com>
2022-02-20 14:31:13 +0700

    Try to fix...

--- index.js
+++ index.js
@@ -7,41 +7,41 @@ const {
   node: { decodeImage },
 } = require("@tensorflow/tfjs-node");
 
-nsfw.load("https://nsfwjs.com/quant_nsfw_mobilenet/").then((model) => {
-  app.use(express.json());
+app.use(express.json());
 
-  app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
-  });
-
-  app.post("/", async (req, res) => {
-    try {
-      const { image } = req.body;
-      if (!image) return res.status(400).send("Image is required");
-
-      const pic =
-        /^([A-Za-z0-9+\/]{4})*([A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}==)?$/gm.test(
-          image
-        )
-          ? Buffer.from(image, "base64")
-          : (
-              await axios.get(image, {
-                responseType: "arraybuffer",
-              })
-            ).data;
-
-      const tfImage = decodeImage(pic, 3);
-      const predictions = await model.classify(tfImage);
-      tfImage.dispose();
-
-      res.send(predictions);
-    } catch (error) {
-      console.log(error);
-      if (!res.headersSent) res.sendStatus(500);
-    }
-  });
-
-  const port = process.env.PORT || 5000;
-
-  app.listen(port, () => console.log(`Server is listening on port ${port}`));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", async (req, res) => {
+  try {
+    const model = await nsfw.load("https://nsfwjs.com/quant_nsfw_mobilenet/");
+
+    const { image } = req.body;
+    if (!image) return res.status(400).send("Image is required");
+
+    const pic =
+      /^([A-Za-z0-9+\/]{4})*([A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}==)?$/gm.test(
+        image
+      )
+        ? Buffer.from(image, "base64")
+        : (
+            await axios.get(image, {
+              responseType: "arraybuffer",
+            })
+          ).data;
+
+    const tfImage = decodeImage(pic, 3);
+    const predictions = await model.classify(tfImage);
+    tfImage.dispose();
+
+    res.send(predictions);
+  } catch (error) {
+    console.log(error);
+    if (!res.headersSent) res.sendStatus(500);
+  }
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
